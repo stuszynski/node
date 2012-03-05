263eb406dcae547a94320d3c48128bf8a6551ad7
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-05 15:06:19 +0100

    Serwowanie statycznej strony appliction.css

--- fortunka/fortunka-03.js
+++ fortunka/fortunka-03.js
@@ -5,7 +5,8 @@ var express = require('express'),
 
 app.set('view engine', 'ejs');
 
-app.use('/public', express.static(__dirname + '/public'));
+// app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 app.listen(8000);
@@ -42,7 +43,7 @@ app.get('/', function(req, res) {
       'title': title,
       'header': header,
       'fortunes': fortunes,
-      'stylesheets': ['/public/application.css']
+      'stylesheets': ['application.css']
     }
   });
 });
