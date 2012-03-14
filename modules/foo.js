b96bf970ada6a0780aadad63300a5be5d20fdc19
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-14 20:53:52 +0100

    modules

---
+++ modules/foo.js
@@ -0,0 +1,4 @@
+var circle = require('./circle.js');
+
+console.log('The area of a circle of radius 4 is', circle.area(4));
+console.log('The circumference of a circle of radius 4 is', circle.circumference(4));
