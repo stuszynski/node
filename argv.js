432675746a445bff83de4956a7826133607d33f2
Wlodek Bzyl <matwb@ug.edu.pl>
2012-03-14 21:01:41 +0100

    modules

--- argv.js
+++ argv.js
@@ -1,3 +1,14 @@
+console.log('PROCESS: argv');
+
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
+
+console.log('PATH: join, dirname, basename');
+
+var path = require('path');
+
+console.log(path.join('/foo', 'bar', 'baz/asdf', 'quux', '..'));
+console.log(path.dirname('/foo/bar/baz/asdf/quux'));
+console.log(path.basename('/foo/bar/baz/asdf/quux.html'));
+console.log(path.basename('/foo/bar/baz/asdf/quux.html', '.html'));
